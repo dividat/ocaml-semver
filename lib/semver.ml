3c7e3eecce766394c9caf1c9df46f3f17aab3941
Emilien Taque <etaque@gmail.com>
2019-05-03 14:28:20 +0200

    Fix comparison and parsing to strictly follow specs. Improve docs.

--- lib/semver.ml
+++ lib/semver.ml
@@ -11,7 +11,7 @@ type t = {
 let mk_version major minor patch prerelease build =
   { major; minor; patch; prerelease; build }
 
-let to_string (v: t) =
+let to_string v =
   let print_series mark identifiers =
     match identifiers with
     | [] -> ""
@@ -20,57 +20,130 @@ let to_string (v: t) =
   string_of_int v.major ^ "." ^ string_of_int v.minor ^ "." ^ string_of_int v.patch ^
   print_series "-" v.prerelease ^ print_series "+" v.build
 
-let is_num0 = function '0' .. '9' -> true | _ -> false
-let is_num1 = function '1' .. '9' -> true | _ -> false
+let is_digit = function '0' .. '9' -> true | _ -> false
 
-let nat =
-  lift2 (^) (satisfy is_num1 >>| Char.escaped) (take_while is_num0) <|> (satisfy is_num0 >>| Char.escaped)
-  >>= fun str -> try return (int_of_string str) with _ -> fail ("invalid number: " ^ str)
+let no_leading_zero str =
+  String.length str <= 1 || String.get str 0 != '0'
+
+let nat: int Angstrom.t =
+  take_while1 is_digit >>= fun str ->
+  if no_leading_zero str then
+    try return (int_of_string str) with _ -> fail ("invalid version number: " ^ str)
+  else
+    fail ("leading 0 in version number: " ^ str)
+
+let isOk = function
+  | Ok _ -> true
+  | Error _ -> false
+
+let%test "natural number is ok" =
+  parse_string (nat <* end_of_input) "123" |> isOk
+
+let%test "number with leading zero fails" =
+  parse_string (nat <* end_of_input) "0123" |> isOk |> not
+
+let%test "0 is ok" =
+  parse_string (nat <* end_of_input) "0" |> isOk
+
+let%test "alphanum fails" =
+  parse_string (nat <* end_of_input) "0hey" |> isOk |> not
 
 let dot =
   char '.'
 
-let identifier =
+let base_identifier =
   take_while1 (function '-' | 'a' .. 'z' | 'A' .. 'Z' | '0' .. '9' -> true | _ -> false)
 
-let identifier_list sep =
-  (char sep) *> (sep_by (char '.') identifier) <|> (return [])
+let prerelease_identifier =
+  let verify_numeric str =
+    try
+      let _ = int_of_string str in
+      if no_leading_zero str then return str else fail ("leading 0 in prerelease numeric identifier: " ^ str)
+    with
+    _ -> return str
+  in
+  base_identifier >>= verify_numeric
 
-let version_parser = (lift4 mk_version nat (dot *> nat) (dot *> nat) (identifier_list '-') <*> (identifier_list '+')) <* end_of_input
+let identifier_list id_parser sep =
+  char sep *> sep_by1 dot id_parser <|> return []
 
-let of_string (str: string) : t =
+let version_parser =
+  (lift4 mk_version
+     nat
+     (dot *> nat)
+     (dot *> nat)
+     (identifier_list prerelease_identifier '-')
+   <*> (identifier_list base_identifier '+'))
+  <* end_of_input
+
+let of_string str =
   match parse_string version_parser str with
-  | Ok v -> v
-  | Error msg -> failwith msg
+  | Ok v -> Some v
+  | Error _ -> None
 
 let is_valid str =
   match parse_string version_parser str with
   | Ok _ -> true
   | Error _ -> false
 
-let compare_identifiers i1 i2 =
-  match (parse_string (nat <* end_of_input) i1, parse_string (nat <* end_of_input) i2) with
-  | (Ok n1, Ok n2) -> compare n1 n2
-  | _ -> compare i1 i2
+(** Prerelease precedence rules:
 
-let rec compare_prerelease is_first p1 p2 =
-  match (p1, p2) with
-  | ([], []) -> 0
-  | ([], _) -> if is_first then 1 else -1
-  | (_, []) -> if is_first then -1 else -1
-  | (h1 :: t1, h2 :: t2) -> if h1 == h2 then compare_prerelease false t1 t2 else compare h1 h2
+    * Identifiers consisting of only digits are compared numerically and
+      identifiers with letters or hyphens are compared lexically in ASCII sort
+      order.
 
-let compare v1 v2 =
-  if v1.major != v2.major then
-    compare v1.major v2.major
-  else if v1.minor != v2.minor then
-    compare v1.minor v2.minor
-  else if v1.patch != v2.patch then
-    compare v1.patch v2.patch
-  else compare_prerelease true v1.prerelease v2.prerelease
+    * Numeric identifiers always have lower precedence than non-numeric
+      identifiers.
 
-let less_than v1 v2 =
-  compare v1 v2 < 0
+    * A larger set of pre-release fields has a higher precedence than a smaller
+      set, if all of the preceding identifiers are equal.
 
-let greater_than v1 v2 =
-  compare v1 v2 > 0
+   <https://semver.org/#spec-item-11>
+*)
+let compare_identifiers ia ib =
+  match parse_string (nat <* end_of_input) ia, parse_string (nat <* end_of_input) ib with
+  | Ok na, Ok nb -> compare na nb
+  | Ok _, _ -> -1
+  | _, Ok _ -> 1
+  | _ -> compare ia ib
+
+let%test "numeric lower than non-numeric" =
+  compare_identifiers "1" "beta" == -1
+
+let%test "numeric comparison" =
+  compare_identifiers "2" "11" == -1
+
+let%test "alphabetical comparison" =
+  compare_identifiers "alpha" "beta" == -1
+
+let rec compare_prerelease is_first pra prb =
+  match pra, prb with
+  | [], [] -> 0
+  | [], _ -> if is_first then 1 else -1
+  | _, [] -> if is_first then -1 else 1
+  | ia :: ta, ib :: tb ->
+    if compare ia ib != 0 then
+      compare_identifiers ia ib
+    else
+      compare_prerelease false ta tb
+
+let%test "larger set has higher precedence" =
+  compare_prerelease true ["alpha"] ["alpha"; "1"] == -1
+
+let%test "compare first non-equal identifiers" =
+  compare_prerelease true ["alpha"; "1"] ["alpha"; "beta"] == -1
+
+let compare a b =
+  if a.major != b.major then
+    compare a.major b.major
+  else if a.minor != b.minor then
+    compare a.minor b.minor
+  else if a.patch != b.patch then
+    compare a.patch b.patch
+  else compare_prerelease true a.prerelease b.prerelease
+
+let less_than a b =
+  compare a b < 0
+
+let greater_than a b =
+  compare a b > 0
--- lib_test/test_semver.ml
+++ lib_test/test_semver.ml
@@ -1,20 +1,37 @@
 open OUnit
 open Semver
 
-let (valids, invalids, sorted) =
+let valids, invalids, sorted =
   let json = Yojson.Basic.from_file "fixtures.json" in
   let open Yojson.Basic.Util in
   let read_list key = json |> member key |> to_list |> List.map to_string in
-  (read_list "valid", read_list "invalid", read_list "compare")
+  read_list "valid", read_list "invalid", read_list "compare"
 
 let test_valids () =
-  List.iter (fun v -> assert_equal (to_string (of_string v)) v) valids
+  let assert_iso v =
+    match of_string v with
+    | Some v' -> assert_equal (to_string v') v
+    | None -> assert_failure ("valid version detected as invalid: " ^ v)
+  in
+  List.iter assert_iso valids
 
 let test_invalids () =
   List.iter (fun v -> assert_bool (v ^ " is invalid") (not (is_valid v))) invalids
 
 let test_compare () =
-  assert_equal sorted (sorted |> List.map of_string |> List.sort compare |> List.map to_string)
+  let rec of_string_list parsed raw =
+    match raw with
+    | [] -> parsed
+    | hd :: tl -> match of_string hd with
+      | Some v -> of_string_list (v :: parsed) tl
+      | None -> of_string_list parsed tl
+  in
+  let list_printer lst =
+    "\n[\n" ^ (lst |> List.map (fun s -> "  " ^ s ^ ";\n") |> String.concat "") ^ "]\n"
+  in
+  assert_equal ~printer:list_printer
+    sorted
+    (sorted |> of_string_list [] |> List.sort compare |> List.map to_string)
 
 let suite =
   "semver suite" >:::
